9de6835e32d9a065c8e1605552f552af07e6661b
Alpha119 <93892825+Alpha119@users.noreply.github.com>
2022-01-07 20:56:21 +0530

    Update admin.py

--- Code_X_Mania/bot/plugins/admin.py
+++ Code_X_Mania/bot/plugins/admin.py
@@ -19,13 +19,13 @@ broadcast_ids = {}
 @StreamBot.on_message(filters.command("status") & filters.private & filters.user(Var.OWNER_ID) & ~filters.edited)
 async def sts(c: Client, m: Message):
     total_users = await db.total_users_count()
-    await m.reply_text(text=f"**Total Users in DB:** `{total_users}`", parse_mode="Markdown", quote=True)
+    await m.reply_text(text=f"**Numbers of users:** `{total_users}`", parse_mode="Markdown", quote=True)
 
 
 @StreamBot.on_message(filters.command("broadcast") & filters.private & filters.user(Var.OWNER_ID) & filters.reply & ~filters.edited)
 async def broadcast_(c, m):
     all_users = await db.get_all_users()
-    broadcast_msg = m
+    broadcast_msg = m.reply_to_message
     while True:
         broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
         if not broadcast_ids.get(broadcast_id):
